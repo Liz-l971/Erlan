f6a3c75d988967e1ce3b0df1cef8bbbb3a93c417
Liz_I971 <ivanovaliza0053@gmail.com>
2024-06-16 21:09:19 +0300

    old_head_test_two

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -192,6 +192,24 @@
                     </button>
                 </form>
                 <?endif;?>
+                <?if(session()->has('lang') && session('lang') == 0):?>
+
+                <form action="/changeValute" method="POST" class="nav_item" style="display: flex;">
+                    @csrf
+                  
+                    <input type="hidden" name="lang" value="1">
+                    
+                    <button type="submit" class="nav_item" tyle="display: flex;" style="background: transparent">
+                        <svg width="32" class="desktop" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M16 31C24.2843 31 31 24.2843 31 16C31 7.71573 24.2843 1 16 1C7.71573 1 1 7.71573 1 16C1 24.2843 7.71573 31 16 31Z" stroke="white" stroke-width="2" />
+                            <path d="M11 19.2H16.1429M12.7143 16V9.92C12.7143 9.6224 12.7143 9.4736 12.7349 9.3504C12.7913 9.01629 12.9598 8.70747 13.216 8.46829C13.4723 8.22912 13.8032 8.0719 14.1611 8.0192C14.2914 8 14.4526 8 14.7714 8H18.7143C19.8509 8 20.941 8.42143 21.7447 9.17157C22.5485 9.92172 23 10.9391 23 12C23 13.0609 22.5485 14.0783 21.7447 14.8284C20.941 15.5786 19.8509 16 18.7143 16H12.7143ZM12.7143 16V24M12.7143 16H11" stroke="white" stroke-width="2" stroke-linecap="round" />
+                        </svg>
+                            <p class="name_valute">
+                                RUB.
+                            </p>
+                    </button>
+                </form>
+                <?endif;?>
                 <div class="nav_item" style="display: flex;">
                     <svg width="28" height="28" class="desktop" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M13.7498 6.1001V21.4001M17.5748 10.5626C17.5748 8.8031 15.8625 7.3751 13.7498 7.3751C11.6371 7.3751 9.9248 8.8031 9.9248 10.5626C9.9248 12.3221 11.6371 13.7501 13.7498 13.7501C15.8625 13.7501 17.5748 15.1781 17.5748 16.9376C17.5748 18.6971 15.8625 20.1251 13.7498 20.1251C11.6371 20.1251 9.9248 18.6971 9.9248 16.9376" stroke="white" stroke-width="2" stroke-linecap="round" />
