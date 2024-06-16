560c83a9e5a707826098c17c242381e5da8b2fa0
Liz_I971 <ivanovaliza0053@gmail.com>
2024-06-16 15:02:35 +0300

    mail

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -1,12 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+
+@extends('loyauts.app')
+@section('title')
+Erlan
+@endsection
+@section('content')
+
+    <div class="right_bilet">
+        <div class="content_bron">
+            <div class="hrum_hrum">
+        
     <h1>{{ $subject }}</h1>
     <p>{{ $body }}</p>
-</body>
-</html>
+
+            </div>
+        </div>
+    </div>
+@endsection
